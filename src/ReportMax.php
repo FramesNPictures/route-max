882c8bbbce587e50d3f12a8c7965246606dc9a07
Chris Cynarski <krzysztof@cynarski.net>
2019-03-12 17:42:03 +0000

    Fix serialization.

--- src/ReportMax.php
+++ src/ReportMax.php
@@ -4,7 +4,6 @@ namespace Fnp\RouteMax;
 
 use Fnp\Dto\Common\DtoToArray;
 use Fnp\Dto\Common\DtoToJson;
-use Fnp\Dto\Common\Flags\DtoFillFlags;
 use Illuminate\Http\Request;
 
 abstract class ReportMax extends RouteMax
@@ -28,10 +27,5 @@ abstract class ReportMax extends RouteMax
     public function handle()
     {
         return $this;
-    }
-
-    public function __toString()
-    {
-        return $this->toJson();
     }
 }
