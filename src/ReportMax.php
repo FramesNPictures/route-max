ab239298b44f3303297057dd05c7a983e7773e19
Chris Cynarski <krzysztof@cynarski.net>
2019-03-14 11:24:14 +0000

    Simplify RouteMax, move automation to ReportMax. Newer version of DTO Library

--- src/ReportMax.php
+++ src/ReportMax.php
@@ -2,12 +2,17 @@
 
 namespace Fnp\RouteMax;
 
+use Fnp\Dto\Common\DtoFill;
 use Fnp\Dto\Common\DtoToArray;
 use Fnp\Dto\Common\DtoToJson;
+use Fnp\Dto\Common\Flags\DtoFillFlags;
+use Fnp\Dto\Common\Helper\Obj;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\App;
 
 abstract class ReportMax extends RouteMax
 {
+    use DtoFill;
     use DtoToArray;
     use DtoToJson;
 
@@ -19,13 +24,55 @@ abstract class ReportMax extends RouteMax
      */
     public function __construct($data = [], Request $request = NULL)
     {
-        parent::__construct($request);
+        $this->initTraits();
 
-        $this->fill($data);
+        $route = [];
+
+        if ($request) {
+            array_merge($data, $request->all());
+
+            if ($request->route())
+                $route = $request->route()->parameters();
+        }
+
+        $this->fill($data, DtoFillFlags::FILL_PUBLIC);
+        $this->fill(
+            $route,
+            DtoFillFlags::FILL_PUBLIC + DtoFillFlags::FILL_PROTECTED
+        );
+    }
+
+    /**
+     * Boot all of the bootable traits on the model.
+     *
+     * @return void
+     */
+    protected function initTraits()
+    {
+        $class = static::class;
+
+        $booted = [];
+
+        foreach (class_uses_recursive($class) as $trait) {
+
+            $method = Obj::methodName('init', class_basename($trait), 'Trait');
+
+            if (method_exists($class, $method) && !in_array($method, $booted)) {
+                App::call([$this, $method]);
+
+                $booted[] = $method;
+            }
+        }
     }
 
     public function handle()
     {
         return $this;
+    }
+
+    public function __invoke(...$args)
+    {
+        $this->fill($args, DtoFillFlags::FILL_PUBLIC + DtoFillFlags::FILL_PROTECTED);
+        $this->handle();
     }
 }
